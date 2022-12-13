c282b486247ba06d16a5e17e4318be85551600fb
nikit0stos <119962184+nikit0stos@users.noreply.github.com>
2022-12-13 20:07:47 +0000

    Udate TiltedTextInputs

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 const Input = styled.input`
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border: 1px solid #E2E8F0;
+    border-radius: 24px;
+    background: #F8FAFC;
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -13,7 +13,7 @@ const Option = styled.div`
 function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
-            <Option className='flex-grow-1 p-2'>{props.children}</Option>
+            <Option className='flex-end-1 p-2'>{props.children}</Option>
             <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
             <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
         </div>
